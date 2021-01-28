dce7ae23dcdc7a5f9a10854da70d96a4bb1f37e8
Paulo Castro <phm.castro@hotmail.com>
2021-01-27 23:17:37 -0300

    added meta description

--- src/components/MyHead/index.js
+++ src/components/MyHead/index.js
@@ -7,6 +7,7 @@ export default function MyHead({ previewImageURL }) {
         <Head>
             <meta property="og:title" content="The Office's Dunder Mifflin Quiz" />
             <meta property="og:type" content="video.other" />
+            <meta property="og:description" content="Who does Michael hates the most in the office? Know the answer? Come test your 'office' skills!" />
             <meta property="og:url" content="https://dunder-mifflin-quiz.phmcastro702.vercel.app/" />
             <meta property="og:image" content={previewImageURL} />
         </Head>
