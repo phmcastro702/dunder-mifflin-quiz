c3a65b023d6985248a25f921b98a92710bbf5605
Paulo Castro <phm.castro@hotmail.com>
2021-01-27 23:24:24 -0300

    meta description 2

--- src/components/MyHead/index.js
+++ src/components/MyHead/index.js
@@ -5,6 +5,10 @@ import Image from 'next/image'
 export default function MyHead({ previewImageURL }) {
     return (
         <Head>
+
+            <meta name="title" content="The Office's Dunder Mifflin Quiz" />
+            <meta name="description" content="Who does Michael hates the most in the office? Know the answer? Come test your 'office' skills!" />
+
             <meta property="og:title" content="The Office's Dunder Mifflin Quiz" />
             <meta property="og:type" content="video.other" />
             <meta property="og:description" content="Who does Michael hates the most in the office? Know the answer? Come test your 'office' skills!" />
